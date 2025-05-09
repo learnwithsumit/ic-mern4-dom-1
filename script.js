eb5b62b24db2c1c275245167f6b117cd68dcadc0
Sumit Saha <sumit@learnwithsumit.com>
2025-05-09 21:41:37 +0600

    class 2

--- script.js
+++ script.js
@@ -1,6 +1,65 @@
-const choToVaiElement = document.createElement("div");
-choToVaiElement.innerHTML = "<span>Choto Vai</span>";
+function completeTask(e) {
+    const checkBox = e.target;
+    const taskItem = checkBox.parentNode;
 
-const container = document.querySelector(".todo-list");
+    const label = taskItem.querySelector("label");
+    const taskName = label.textContent;
 
-container.appendChild(choToVaiElement);
+    const taskToAdd = generateCompleteTodo(taskName);
+    document.querySelector(".complete-list ul").appendChild(taskToAdd);
+
+    taskItem.parentNode.removeChild(taskItem);
+}
+
+function deleteTask(e) {
+    const taskItem = e.target.parentNode;
+    const taskContainer = taskItem.parentNode;
+
+    taskContainer.removeChild(taskItem);
+}
+
+// generate incomplete todo
+function generateIncompleteTodo(taskName) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+
+    const inputItem = document.createElement("input");
+    inputItem.setAttribute("type", "checkbox");
+    inputItem.onchange = completeTask;
+
+    const label = document.createElement("label");
+    label.innerText = taskName;
+
+    listItem.append(inputItem, label);
+
+    return listItem;
+}
+
+// generate complete todo
+function generateCompleteTodo(taskName) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+
+    const button = document.createElement("button");
+    button.classList.add("delete");
+    button.innerText = "Delete";
+    button.onclick = deleteTask;
+
+    listItem.append(taskName, " ", button);
+
+    return listItem;
+}
+
+const todoForm = document.querySelector("form");
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskInput = document.querySelector("#new-task");
+    const taskName = taskInput.value;
+
+    const listItemToAdd = generateIncompleteTodo(taskName);
+    const listContainer = document.querySelector("#items");
+
+    listContainer.appendChild(listItemToAdd);
+
+    taskInput.value = "";
+});
